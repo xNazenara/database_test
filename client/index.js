a857bf46baa21fd2ec12048e56743ee771c896d5
xNazenara <kanlandball553@gmail.com>
2020-08-21 05:51:16 +0300

    post create client

--- client/index.js
+++ client/index.js
@@ -1,1 +1,42 @@
-axios.get('http://localhost:3000/name').then(res=>alert(res.data))
+
+const addButton = document.querySelector('.add-button')
+const closeButton = document.querySelector('.input-button-close')
+const inputContainer = document.querySelector('.input-container')
+const inputTitle = document.querySelector('.input-title-value')
+const inputText = document.querySelector('.input-text-value')
+const createButton = document.querySelector('.input-button-add')
+let titleValue = ''
+let textValue = ''
+addButton.addEventListener('click', (e) => {
+  const invisible = inputContainer.classList.contains('hidden')
+  if (invisible) {
+    inputContainer.classList.remove("hidden")
+  }
+})
+
+closeButton.addEventListener('click', (e) => {
+  const visible = !inputContainer.classList.contains('hidden')
+  if (visible) {
+    inputContainer.classList.add("hidden")
+  }
+})
+
+inputTitle.addEventListener('input', (e) => {
+   titleValue = e.target.value
+})
+
+inputText.addEventListener('input', (e) => {
+  textValue = e.target.value
+})
+
+createButton.addEventListener('click', (e) => {
+  axios.post('http://localhost:3000/post-create', {
+    textValue: textValue,
+    titleValue: titleValue
+  }).then(res=>alert(res.data))
+  textValue = ''
+  titleValue = ''
+  inputTitle.value = ''
+  inputText.value = ''
+  inputContainer.classList.add("hidden")
+})
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const cors = require("cors")
 const { Client } = require('pg')
 
@@ -6,16 +7,21 @@ const app = express()
 
 app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const connectionString = 'postgres://postgres:123@localhost:5432/postgres'
 
 const client = new Client({connectionString: connectionString})
 
 client.connect()
 
-app.get('/name', (req, res) => {
-  client.query('select * from users', (err, data) => {
-    res.send(data.rows)
-  })
+app.post('/post-create', (req, res) => {
+  // client.query('select * from users', (err, data) => {
+  //   res.send(data.rows)
+  // })
+  console.log(req.body)
+  res.send('234')
 })
 
 app.listen(3000, ()=>console.log("Started"))
